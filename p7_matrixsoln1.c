9bd99ac02b84919ce79bff022858574e8815932f
syamilisn <shyamilisn.itfa@gmail.com>
2022-11-02 04:29:03 +0530

    added solution in P7

--- p7_matrixsoln1.c
+++ p7_matrixsoln1.c
@@ -5,6 +5,9 @@
 void inputMatrix(int matrix[][COLS], int rows, int cols);
 void printMatrix(int matrix[][COLS], int rows, int cols);
 void addMatrix(int (matrix1)[][COLS], int (matrix2)[][COLS] , int rows, int cols);
+void mulMatrix(int (matrix1)[][COLS], int (matrix2)[][COLS] , int rows, int cols);
+void transposeMatrix(int (matrix1)[][COLS], int rows, int cols);
+
 
 int main()
 {
@@ -33,6 +36,11 @@ int main()
     /* Matrix Addition*/
     addMatrix(matrix1,matrix2,ROWS, COLS);
 
+    /* Matrix Multiplication*/
+    mulMatrix(matrix1,matrix2,ROWS, COLS);
+
+    /* Transpose of a Matrix*/
+    transposeMatrix(matrix1,ROWS,COLS);
     return 0;
 }
 
@@ -67,10 +75,37 @@ void addMatrix(int (matrix1)[][COLS], int (matrix2)[][COLS] , int rows, int cols
         for (j = 0; j < cols; j++)
         {
             matrixSum[i][j] = *(*(matrix1 + i) + j) + *(*(matrix2 + i) + j);
-            //printf("%d ", *(*(matrix + i) + j));
         }
         printf("\n");
     }
     printMatrix(matrixSum,rows,cols);
 }
-//void addMatrix(int (*matrix1)[][COLS], int (*matrix2)[][COLS], int rows, int cols){}
+
+void mulMatrix(int (matrix1)[][COLS], int (matrix2)[][COLS] , int rows, int cols){
+    int i, j, k, matrixMul[ROWS][COLS];
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            matrixMul[i][j] = 0;
+            for(k = 0; k < cols; k++)
+                matrixMul[i][j] += (*(*(matrix1 + i) + k)) * (*(*(matrix2 + k) + j));
+        }
+        printf("\n");
+    }
+    printMatrix(matrixMul,rows,cols);
+}
+
+
+void transposeMatrix(int (matrix1)[][COLS] , int rows, int cols){
+    int i, j, transpose[ROWS][COLS];
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            transpose[j][i] = *(*(matrix1 + i) + j);
+        }
+        printf("\n");
+    }
+    printMatrix(transpose,rows,cols);
+}
